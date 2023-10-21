8670afd23af0aa6b87a3197b2cec21cfc2a73d4a
Katariina Tammi <katariina.tammi@tptlive.ee>
2023-10-21 12:24:34 +0300

    1

---
+++ main.js
@@ -0,0 +1,139 @@
+const tasks = [
+    {
+        id: 1,
+        name: 'Task 1',
+        completed: false
+    },
+    {
+        id: 2,
+        name: 'Task 2',
+        completed: true
+    }
+];
+let lastTaskId = 2;
+
+let taskList;
+let addTask;
+let saveAllButton;
+// kui leht on brauseris laetud siis lisame esimesed taskid lehele
+
+
+window.addEventListener('load', () => {
+    taskList = document.querySelector('#task-list');
+    addTask = document.querySelector('#add-task');
+    saveAllButton = document.querySelector('#save-all');
+    saveAllButton.addEventListener('click', () => {
+        saveAllTasks(); 
+    });
+
+    // Add this section after defining the addTask button
+    // kui nuppu vajutatakse siis lisatakse uus task
+    addTask.addEventListener('click', () => {
+        const task = createTask();
+        const taskRow = createTaskRow(task);
+        taskList.appendChild(taskRow);
+    });
+
+});
+
+
+
+function renderTask(task) {
+    const taskRow = createTaskRow(task);
+    taskList.appendChild(taskRow);
+}
+
+function createTask() {
+    lastTaskId++;
+    const task = {
+        id: lastTaskId,
+        name: 'Task ' + lastTaskId,
+        completed: false
+    };
+    tasks.push(task);
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", "Bearer mErAbJ0UQaJHWQcYSquHtbzu2aQ7GJFU");
+    
+    var raw = JSON.stringify({
+      "title": "123",
+      "desc": ""
+    });
+    
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+    
+    fetch("http://demo2.z-bit.ee/tasks", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+
+    return task;
+}
+
+function createTaskRow(task) {
+    let taskRow = document.querySelector('[data-template="task-row"]').cloneNode(true);
+    taskRow.removeAttribute('data-template');
+
+    // Täidame vormi väljad andmetega
+    const name = taskRow.querySelector("[name='name']");
+    name.innerText = task.name;
+
+    const checkbox = taskRow.querySelector("[name='completed']");
+    checkbox.checked = task.completed;
+
+    const deleteButton = taskRow.querySelector('.delete-task');
+    deleteButton.addEventListener('click', () => {
+        taskList.removeChild(taskRow);
+        tasks.splice(tasks.indexOf(task), 1);
+    });
+
+    // Valmistame checkboxi ette vajutamiseks
+    hydrateAntCheckboxes(taskRow);
+
+    return taskRow;
+}
+
+
+function createAntCheckbox() {
+    const checkbox = document.querySelector('[data-template="ant-checkbox"]').cloneNode(true);
+    checkbox.removeAttribute('data-template');
+    hydrateAntCheckboxes(checkbox);
+    return checkbox;
+}
+
+/**
+ * See funktsioon aitab lisada eridisainiga checkboxile vajalikud event listenerid
+ * @param {HTMLElement} element Checkboxi wrapper element või konteiner element mis sisaldab mitut checkboxi
+ */
+function hydrateAntCheckboxes(element) {
+    const elements = element.querySelectorAll('.ant-checkbox-wrapper');
+    for (let i = 0; i < elements.length; i++) {
+        let wrapper = elements[i];
+
+        // Kui element on juba töödeldud siis jäta vahele
+        if (wrapper.__hydrated)
+            continue;
+        wrapper.__hydrated = true;
+
+
+        const checkbox = wrapper.querySelector('.ant-checkbox');
+
+        // Kontrollime kas checkbox peaks juba olema checked, see on ainult erikujundusega checkboxi jaoks
+        const input = wrapper.querySelector('.ant-checkbox-input');
+        if (input.checked) {
+            checkbox.classList.add('ant-checkbox-checked');
+        }
+        
+        // Kui checkboxi või label'i peale vajutatakse siis muudetakse checkboxi olekut
+        wrapper.addEventListener('click', () => {
+            input.checked = !input.checked;
+            checkbox.classList.toggle('ant-checkbox-checked');
+        });
+    }
+}
